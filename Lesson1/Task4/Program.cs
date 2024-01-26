1ff9c657f604d28232c2a299542391f6c9fed892
Vitaly Yarnov <vyarnov@gmail.com>
2024-01-26 16:06:26 +0300

    add new Task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 6;
+int b = 8;
+int c = 3;
+int d = 9;
+int e = 11;
+int max = a;
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+if (max < d)
+{
+    max = d;
+}
+if (max < e)
+{
+    max = e;
+}
+Console.WriteLine(max);
