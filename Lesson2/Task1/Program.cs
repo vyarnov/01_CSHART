360b6db2754be0f3a08c2d3db56eda20a24e27a0
Vitaly Yarnov <vyarnov@gmail.com>
2024-01-31 15:13:32 +0300

    add new Lesso2 to 01_CSHART

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[5];
+array[0] = 9;
+array[1] = 2;
+array[2] = 3;
+array[3] = 5;
+array[4] = 7;
+Console.WriteLine(array[0]);
+Console.WriteLine(array[1]);
+Console.WriteLine(array[2]);
+Console.WriteLine(array[3]);
+Console.WriteLine(array[4]);
+// Console.WriteLine(array[5]);
+
+int[] array2 = new int[5] { 1, 2, 3, 4, 5 };
+
+int[] array3 = { 1, 2, 3, 4, 5 };
+
