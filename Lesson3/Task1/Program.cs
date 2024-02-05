f10970427bfa4782f10a9e3ae1fbf94a41ac19c4
Vitaly Yarnov <vyarnov@gmail.com>
2024-02-05 17:36:05 +0300

    add some text to Task1 in Lesson3

--- Lesson3/Task1/Program.cs
+++ Lesson3/Task1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int a = 1;
+// int b = 2;
+// int c = 3;
+// int d = 4;
+// double numenator = 10;
+// int denumenator = 0;
+// double result = 0;
+
+
+
+int CalculateFormula(int a, int b)
+{
+    int numenator = a * b;
+    // int denumenator = c + d;
+    // double result = numenator / denumenator;
+    return numenator;
+}
+
+int numenator = 0;
+CalculateFormula(1,2);
+Console.Write(numenator);
